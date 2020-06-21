60daa869a2574fd2d9966cffcd804a3d007ab47b
mserloth <61293869+mserloth@users.noreply.github.com>
2020-06-21 19:05:29 +0200

    Korr.

--- resources/views/film/edit.blade.php
+++ resources/views/film/edit.blade.php
@@ -4,8 +4,7 @@
 @section('content')
     <section class="section">
         <div class="container">
-            <h1 class="title is-2">List film <i class="fa fa-film" aria-hidden="true"></i></h1>
+            <h1>Edit film</h1>
         </div>
     </section>
-    <list-film v-bind:enable-edit="true" v-bind:enable-show="true"></list-film>
 @endsection
--- resources/views/film/index.blade.php
+++ resources/views/film/index.blade.php
@@ -4,7 +4,8 @@
 @section('content')
     <section class="section">
         <div class="container">
-            <h1>List film</h1>
+            <h1 class="title is-2">List film <i class="fa fa-film" aria-hidden="true"></i></h1>
         </div>
     </section>
+    <list-film v-bind:enable-edit="true" v-bind:enable-show="true"></list-film>
 @endsection
