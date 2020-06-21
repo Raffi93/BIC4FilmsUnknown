d8af3eb23147a17bd44564f8ecd3d5e46bdf8c57
ic18b002 <ic18b002@technikum-wien.at>
2020-06-21 21:06:46 +0200

    changed index.blade in film and actor

--- resources/views/actor/index.blade.php
+++ resources/views/actor/index.blade.php
@@ -7,5 +7,14 @@
             <h1 class="title is-2">List actors <i class="fa fa-users" aria-hidden="true"></i></h1>
         </div>
     </section>
-    <list-actor class ="test" v-bind:enable-edit="true" v-bind:enable-show="true" ></list-actor>
+    <div class="container">
+        <input type="checkbox" class="hackbox liste" id="liste_anzeigen">
+        <label class="liste" for="liste_anzeigen"></label>
+        <span class ="test">
+        <list-actor class ="test" v-bind:enable-edit="true" v-bind:enable-show="true" ></list-actor>
+        </span>
+        <span class ="test2">
+        <page-actor class ="test" v-bind:enable-edit="true" v-bind:enable-show="true" ></page-actor>
+        </span>
+    </div>
 @endsection
